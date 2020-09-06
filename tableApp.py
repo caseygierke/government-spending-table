77ee1aa456b5c795a7f16cd02b8f9128a2c095b4
abc <abc@example.com>
2020-09-06 13:07:17 -0600

    Setup a template and added a table with sorting functionality.

--- tableApp.py
+++ tableApp.py
@@ -1,11 +1,13 @@
 # app.py
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+# Define homepage view
 @app.route('/')
-def initial_app():
-    return '<h1>Ummm, is this thing on?</h1>'
+def getData():
+    
+    return render_template("table.html")
 
 # We only need this for local development.
 if __name__ == '__main__':
