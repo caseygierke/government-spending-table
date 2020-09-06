9591fd322b358aebed2a47b5a4455b505708225a
abc <abc@example.com>
2020-09-06 13:58:50 -0600

    Make API request dependent on condition and rendering of table dependent on having data variable from flask.

--- tableApp.py
+++ tableApp.py
@@ -1,18 +1,24 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 app = Flask(__name__)
 
 # Define homepage view
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def getData():
     
-    # Get data from API
-    response = requests.get("https://api.usaspending.gov/api/v2/references/toptier_agencies/")
-    # Convert data to json
-    data = response.json()
-
-    return render_template("table.html", data=data['results'])
+    if request.method == 'POST':
+        
+        # Get data from API
+        response = requests.get("https://api.usaspending.gov/api/v2/references/toptier_agencies/")
+        # Convert data to json
+        data = response.json()
+    
+        return render_template("table.html", data=data['results'])
+        
+    else:
+    
+        return render_template("table.html")
 
 # We only need this for local development.
 if __name__ == '__main__':
