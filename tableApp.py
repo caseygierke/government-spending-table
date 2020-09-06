78f049a8016deef07897d64d656e47d622eeb3fe
abc <abc@example.com>
2020-09-06 13:32:51 -0600

    Added API call to view, sent to template. Looped through API data to build table.

--- tableApp.py
+++ tableApp.py
@@ -1,13 +1,18 @@
-# app.py
 
 from flask import Flask, render_template
+import requests
 app = Flask(__name__)
 
 # Define homepage view
 @app.route('/')
 def getData():
     
-    return render_template("table.html")
+    # Get data from API
+    response = requests.get("https://api.usaspending.gov/api/v2/references/toptier_agencies/")
+    # Convert data to json
+    data = response.json()
+
+    return render_template("table.html", data=data['results'])
 
 # We only need this for local development.
 if __name__ == '__main__':
